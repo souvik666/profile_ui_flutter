fdd0ec0424d8a395bc37022027deb033f7fabeb4
souvik666 <peacedynasty0@gmail.com>
2020-09-08 20:05:44 +0530

    666

---
+++ lib/main.dart
@@ -0,0 +1,141 @@
+import 'package:flutter/material.dart';
+import 'package:profile/widgets/info_card.dart';
+import 'package:url_launcher/url_launcher.dart' as launcher;
+
+const url = 'http:example.com';
+const email = 'example@gmail.com';
+const phone = '+880 xxx xxx';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Home(),
+    );
+  }
+}
+
+class Home extends StatelessWidget {
+  void _showDialog(BuildContext context, {String title, String msg}) {
+    final dialog = AlertDialog(
+      title: Text(title),
+      content: Text(msg),
+      actions: <Widget>[
+        RaisedButton(
+          color: Colors.teal,
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          child: Text(
+            'Close',
+            style: TextStyle(
+              color: Colors.white,
+            ),
+          ),
+        ),
+      ],
+    );
+    showDialog(context: context, builder: (x) => dialog);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            CircleAvatar(
+              radius: 50,
+              backgroundImage: AssetImage('images/img.jpg'),
+            ),
+            Text(
+              'User name',
+              style: TextStyle(
+                fontSize: 40.0,
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+                fontFamily: 'Pacifico',
+              ),
+            ),
+            Text(
+              'User data',
+              style: TextStyle(
+                fontFamily: 'Source Sans Pro',
+                fontSize: 30.0,
+                color: Colors.teal[50],
+                letterSpacing: 2.5,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(
+              height: 20,
+              width: 200,
+              child: Divider(
+                color: Colors.teal.shade700,
+              ),
+            ),
+            InfoCard(
+              text: phone,
+              icon: Icons.phone,
+              onPressed: () async {
+                String removeSpaceFromPhoneNumber =
+                    phone.replaceAll(new RegExp(r"\s+\b|\b\s"), "");
+                final phoneCall = 'tel:$removeSpaceFromPhoneNumber';
+
+                if (await launcher.canLaunch(phoneCall)) {
+                  await launcher.launch(phoneCall);
+                } else {
+                  _showDialog(
+                    context,
+                    title: 'Sorry',
+                    msg: 'Phone number can not be called. Please try again!',
+                  );
+                }
+              },
+            ),
+            InfoCard(
+              text: email,
+              icon: Icons.email,
+              onPressed: () async {
+                final emailAddress = 'mailto:$email';
+
+                if (await launcher.canLaunch(emailAddress)) {
+                  await launcher.launch(emailAddress);
+                } else {
+                  _showDialog(
+                    context,
+                    title: 'Sorry',
+                    msg: 'Email can not be send. Please try again!',
+                  );
+                }
+              },
+            ),
+            InfoCard(
+              text: url,
+              icon: Icons.web,
+              onPressed: () async {
+                if (await launcher.canLaunch(url)) {
+                  await launcher.launch(url);
+                } else {
+                  _showDialog(
+                    context,
+                    title: 'Sorry',
+                    msg: 'URL can not be opened. Please try again!',
+                  );
+                }
+              },
+            ),
+            InfoCard(
+              text: 'User Location',
+              icon: Icons.location_city,
+            ),
+          ],
+        ),
+      ),
+      backgroundColor: Colors.teal[200],
+    );
+  }
+}
